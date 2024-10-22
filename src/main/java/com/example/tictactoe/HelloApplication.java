647df3f0ba96e4d7152e1559aa69ef229d174e7d
EvgeniiaGranina <evgeniia.granina@iths.se>
2024-10-22 19:10:59 +0200

    Add a gaming table

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 518, 440);
+        stage.setTitle("Welcome to Tic-Tac-Toe");
         stage.setScene(scene);
         stage.show();
     }
